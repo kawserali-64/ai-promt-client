f9640982162272381e867960b25f0808be0ec0b6
kawserali-64 <kawserali6436@gmail.com>
2026-06-18 01:40:27 +0600

    signup page added

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import HeroBanner from "@/components/Banner";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">home page</h1>
       <HeroBanner />
     </div>
   );
